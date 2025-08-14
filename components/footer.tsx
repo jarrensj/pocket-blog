85b752811340facb8ccd9736caae3a4df0fe7f9d
jsantibout <joseph.santibout@gmail.com>
2025-08-14 01:17:21 -0700

    added eevee summer blog post (#13)
    * added eevee summer blog post
    
    * Updated disclaimer and added hyperlink to coupon blog

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="pb-8 text-center text-sm text-gray-500">
       This is a fan site and is not affiliated with or endorsed by Nintendo,
       Game Freak, or The Pokémon Company. All trademarks and content belong to
-      their respective owners.
+      their respective owners. We are not responsible for the content of any external links.
     </footer>
   );
 }
